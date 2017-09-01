2e3fe1580067928b6c170273c81653228fa63a37
João Pedro Silveira <joao.pedro0842@hotmail.com>
2017-08-31 22:43:34 -0300

    Add rotations to avl_node

--- PAA-Tree/Tree.cpp
+++ PAA-Tree/Tree.cpp
@@ -26,3 +26,56 @@ void arvore_binaria::add(int value)
 	else
 		root_ = new node(value);
 }
+
+void avl_node::simple_right_rotation(avl_node* node1)
+{
+	const auto node2 = static_cast<avl_node*>(node1->left_);
+
+	node1->left_ = node2->right_;
+	node2->right_ = node1;
+	node1->father_->change_child(node1, node2);
+}
+
+void avl_node::simple_left_rotation(avl_node* node1)
+{
+	const auto node2 = static_cast<avl_node*>(node1->right_);
+
+	node1->right_ = node2->left_;
+	node2->left_ = node1;
+	node1->father_->change_child(node1, node2);
+}
+
+void avl_node::left_right_rotation(avl_node* node1)
+{
+	const auto node2 = static_cast<avl_node*>(node1->left_);
+	const auto node3 = static_cast<avl_node*>(node2->right_);
+
+	node2->right_ = node3->left_;
+	node3->left_ = node2;
+	node1->left_ = node3->right_;
+	node3->right_ = node1;
+
+	node1->change_child(node1, node3);
+}
+
+void avl_node::right_left_rotation(avl_node* node1)
+{
+	const auto node2 = static_cast<avl_node*>(node1->right_);
+	const auto node3 = static_cast<avl_node*>(node2->left_);
+
+	node2->left_ = node3->right_;
+	node3->right_ = node2;
+	node1->right_ = node3->left_;
+	node3->left_ = node1;
+
+	node1->change_child(node1, node3);
+}
+
+
+void avl_node::change_child(avl_node* old_child, avl_node* new_child)
+{
+	if (left_ == old_child)
+		left_ = new_child;
+	else
+		right_ = new_child;
+}
--- PAA-Tree/Tree.h
+++ PAA-Tree/Tree.h
@@ -18,22 +18,30 @@ public:
 	virtual void add(int value);
 };
 
-class avl_node : node
+class avl_node : protected node
 {
+	int balance_;
+	avl_node* father_;
+
+	static void simple_left_rotation(avl_node* node1);
+	static void simple_right_rotation(avl_node* node1);
+	static void left_right_rotation(avl_node* node1);
+	static void right_left_rotation(avl_node* node1);
+
+	void change_child(avl_node* old_, avl_node* new_);
+
 public:
-	avl_node() : node(), balance_(0) {}
-	explicit avl_node(int value) : node(value), balance_(0) {}
-	avl_node(int value, node* left, node* right) : node(value, left, right), balance_(0) {}
-	avl_node(int value, node* left, node* right, int balance) : node(value, left, right), balance_(balance) {}
+	avl_node() : node(), balance_(0), father_(nullptr) {}
+	explicit avl_node(int value) : node(value), balance_(0), father_(nullptr) {}
+	avl_node(int value, avl_node* father) : node(value), balance_(0), father_(father) {}
+	avl_node(int value, node* left, node* right) : node(value, left, right), balance_(0), father_(nullptr) {}
+	avl_node(int value, node* left, node* right, int balance) : node(value, left, right), balance_(balance), father_(nullptr) {}
 
 	int& balance() { return balance_; }
-private:
-	int balance_;
 };
 
 class arvore_binaria
 {
-private:
 	node* root_;
 
 public:
